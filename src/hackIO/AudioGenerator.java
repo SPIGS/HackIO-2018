ab6e23fe42e73ac33db82a8e7738cb70e29b97aa
Brian Sharp <briansharp628@gmail.com>
2018-10-27 15:03:56 -0400

    Finished txt to wav audio

--- src/hackIO/AudioGenerator.java
+++ src/hackIO/AudioGenerator.java
@@ -1,12 +1,15 @@
 package hackIO;
 import java.io.File;
+import java.io.IOException;
 
 public class AudioGenerator {
 	public static Runtime runtime = Runtime.getRuntime();
 	
-	public static File wav(File input)
+	public static File wav(File input) throws IOException
 	{
-		File output = new File("res/wav/" + input.getName() + ".wav");
+		File output = new File("res/wav/" + input.getName().replace(".txt", ".wav") );
+		
+		runtime.exec("cmd /c \"cd dectalk && say -w ../" + output.getPath() + " < ../" + input.getPath() + "\"");
 		
 		return output;
 	}
